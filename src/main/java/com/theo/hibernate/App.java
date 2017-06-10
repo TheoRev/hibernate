8e4017e471352d6c7ef13e74a87f0a29d601e1fa
TheoRev <hrevillafernandez79@gmail.com>
2017-06-10 18:52:19 -0500

    first commit

---
+++ src/main/java/com/theo/hibernate/App.java
@@ -0,0 +1,9 @@
+package com.theo.hibernate;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/com/theo/hibernate/model/Teacher.java
@@ -0,0 +1,14 @@
+package com.theo.hibernate.model;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="teacher")
+public class Teacher implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+
+}
